1a04044a4a7960f3d9b8ca71d371c99204543fa9
Salma Sultana Trisha <90673367+Salma-Trisha@users.noreply.github.com>
2022-06-17 16:20:07 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("menu").style.top = "0";
+  } else {
+    document.getElementById("menu").style.top = "-50px";
+  }
+}
+
+
